8b740420ebe329e3c4dee1999b45f9d279ba65fc
ishizu <demekinn_96@yahoo.co.jp>
2019-06-13 12:14:00 +0900

    add: 投稿フォームを作成

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -6,4 +6,16 @@ class PicturesController < ApplicationController
     @picture = Picture.new
   end
 
+  def create
+    Picture.create(picture_params)
+    flash[:notice] = '投稿しました'
+    #redirect_to pictures_path
+  end
+
+  private
+
+  def picture_params
+    params.require(:picture).permit(:content)
+  end
+
 end
