e3a6a492e0f43d4755ab1cd25fffcf4b6e516cb5
ishizu <demekinn_96@yahoo.co.jp>
2019-06-13 11:34:26 +0900

    add: picturesコントローラのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  root to: 'pictures#index'
   resources :users, only: [:new, :create, :show, :edit, :update]
   resources :sessions, only: [:new, :create, :destroy]
+  resources :pictures do
+    collection do
+      post :confirm
+    end
+    member do
+      patch :confirm
+    end
+  end
 end
