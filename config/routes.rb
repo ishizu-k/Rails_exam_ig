7879e7ba4fe3bfe135c41e03a618398f376059e8
ishizu <demekinn_96@yahoo.co.jp>
2019-06-14 17:15:37 +0900

    update: メニューを変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'pictures#index'
+  root to: 'users#new'
   resources :users, only: [:new, :create, :show, :edit, :update]
   resources :sessions, only: [:new, :create, :destroy]
   resources :pictures do
